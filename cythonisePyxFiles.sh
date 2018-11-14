30c4010146a0f00b3ac7bf3f86e3f6bd1206f980
Guillaume Louvel <guillaume.louvel@normalesup.org>
2018-11-14 20:12:46 +0100

    cythonise adapted for python3

--- cythonisePyxFiles.sh
+++ cythonisePyxFiles.sh
@@ -4,7 +4,13 @@
 
 set -eu
 
-PATH_LIBSDYOGEN=${1}
+PATH_LIBSDYOGEN=${1:-.}
+# Path to the most recent Python3 development headers (apt install python3-dev)
+PATH_PYTHON3HEADER=$(find /usr/include/ -maxdepth 1 -name "python3*" | sort -rV | head -1)
+
+echo "* Working directory: '${PATH_LIBSDYOGEN}'
+* Path to Python 3 development files: '${PATH_PYTHON3HEADER}'" >&2
+
 cython3 ${PATH_LIBSDYOGEN}/extractDiags.pyx
 # C compilation of extractDiags.c into extractDiags.so that can be used as a python package
-gcc -shared -pthread -fPIC -fwrapv -O2 -Wall -fno-strict-aliasing -I/usr/include/python2.7 -o ${PATH_LIBSDYOGEN}/extractDiags.so ${PATH_LIBSDYOGEN}/extractDiags.c
+gcc -shared -pthread -fPIC -fwrapv -O2 -Wall -fno-strict-aliasing -I${PATH_PYTHON3HEADER} -o ${PATH_LIBSDYOGEN}/extractDiags.so ${PATH_LIBSDYOGEN}/extractDiags.c
